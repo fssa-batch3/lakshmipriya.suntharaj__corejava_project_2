6832f5480662aafe210a93e911c1a5785d2f64f3
priya-fssa <118426307+priya-fssa@users.noreply.github.com>
2023-09-07 18:35:37 +0530

    adding command

--- src/main/java/com/fssa/recipe/dao/UserDAO.java
+++ src/main/java/com/fssa/recipe/dao/UserDAO.java
@@ -10,7 +10,14 @@ import com.fssa.recipe.util.Utilities;
 
 public class UserDAO {
 
-	
+	/**
+	 * Authenticates a user by checking their email and password against the database.
+	 *
+	 * @param user The User object containing the email and password for authentication.
+	 * @return True if the user is authenticated, false otherwise.
+	 * @throws SQLException         If a database access error occurs.
+	 * @throws ClassNotFoundException If the required database driver class is not found.
+	 */
 	// Get user from DB
 	public boolean login(User user) throws SQLException, ClassNotFoundException {
 		String query = "SELECT * FROM USER WHERE email = ? AND password = ?";
@@ -26,6 +33,17 @@ public class UserDAO {
 		return rs.next();
 	} 
 	}
+	
+	
+	/**
+	 * Registers a user by inserting their information into the USER table.
+	 *
+	 * @param user The User object containing the user's information.
+	 * @return True if the user was successfully registered, false otherwise.
+	 * @throws SQLException If a database access error occurs.
+	 * @throws ClassNotFoundException If the database driver class is not found.
+	 */
+	
 	
 	public boolean register(User user) throws SQLException, ClassNotFoundException {
 		String query = "INSERT INTO USER (Username ,email,password) VALUES (?,?,?)";
@@ -45,6 +63,17 @@ public class UserDAO {
 	}
 	
 // update user 
+	
+	
+	
+	/**
+	 * Updates the username of a user based on their email address.
+	 *
+	 * @param user The User object containing the updated username and email.
+	 * @return True if the user was successfully updated, false otherwise.
+	 * @throws SQLException If a database access error occurs.
+	 * @throws ClassNotFoundException If the database driver class is not found.
+	 */
 	public boolean updateUser(User user) throws SQLException, ClassNotFoundException {
 		
 		String query = "UPDATE USER SET userName = ? WHERE email = ?";
