a9ed34ac8efc53dbfba7abe893a2261fa9f86716
priya-fssa <118426307+priya-fssa@users.noreply.github.com>
2023-09-07 18:49:19 +0530

    remove unused

--- src/main/java/com/fssa/recipe/dao/RecipeDAO.java
+++ src/main/java/com/fssa/recipe/dao/RecipeDAO.java
@@ -2,7 +2,7 @@ package com.fssa.recipe.dao;
 
 import java.util.ArrayList;
 import java.sql.Connection;
-import java.sql.DriverManager;
+
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
