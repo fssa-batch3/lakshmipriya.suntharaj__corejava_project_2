fed4480d33cffb2ad955d7a1ee220c39b5e80e77
priya-fssa <118426307+priya-fssa@users.noreply.github.com>
2023-09-08 12:00:43 +0530

    connection close

--- src/main/java/com/fssa/recipe/util/Utilities.java
+++ src/main/java/com/fssa/recipe/util/Utilities.java
@@ -5,25 +5,43 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Utilities {
-	public static Connection getConnection() throws ClassNotFoundException {
-		Connection con = null;
-		
-		// Cloud DB
-				final String DB_URL = System.getenv("DB_URL_1");
-				final String DB_USER = System.getenv("DB_USER_1");
-				final String DB_PASSWORD = System.getenv("DB_PASSWORD_1");
+	public static Connection getConnection() throws ClassNotFoundException, SQLException {
+	    Connection con = null;
+	    /**
+	     * Establishes a database connection to a local database using environment variables.
+	     *
+	     * @return Connection object representing the database connection.
+	     * @throws ClassNotFoundException if the MySQL JDBC driver class is not found.
+	     * @throws SQLException if a SQL-related error occurs during connection.
+	     */
+	    // local DB
+	    final String DB_URL = System.getenv("DB_URL_1");
+	    final String DB_USER = System.getenv("DB_USER_1");
+	    final String DB_PASSWORD = System.getenv("DB_PASSWORD_1");
 
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			return con = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
-		} catch (ClassNotFoundException | SQLException e) {
-			
-			
-		}
-		return con;
-		
-
-	} 
+	    try {
+	        
+	        Class.forName("com.mysql.cj.jdbc.Driver");
+	        
+	        con = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+	        
+	        return con;
+	    } catch (ClassNotFoundException e) {
+	        	        throw e;
+	    } catch (SQLException e) {
+	    
+	        throw e;
+	    } finally {
+	        
+	        if (con != null) {
+	            try {
+	                con.close();
+	            } catch (SQLException e) {
+	               
+	            }
+	        }
+	    }
+	}
 	
 	
 	
