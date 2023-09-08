b60009106f49be1de9f46796044b7e5056c5cf3c
priya-fssa <118426307+priya-fssa@users.noreply.github.com>
2023-09-08 18:32:29 +0530

    fixing bugs

--- src/main/java/com/fssa/recipe/util/Utilities.java
+++ src/main/java/com/fssa/recipe/util/Utilities.java
@@ -37,7 +37,7 @@ public class Utilities {
 	            try {
 	                con.close();
 	            } catch (SQLException e) {
-	               
+	            	throw e;
 	            }
 	        }
 	    }
